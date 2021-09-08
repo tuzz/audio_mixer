a03f309ee4efb6eaff70e7bf784c81220728ffff
Chris Patuzzo <chris@patuzzo.co.uk>
2021-09-08 15:51:30 +0100

    Add a note about adding a changed field to the Dynamic structs

--- src/dynamic_controls/dynamic_float.rs
+++ src/dynamic_controls/dynamic_float.rs
@@ -1,5 +1,17 @@
 use crate::*;
 
+// TODO: consider adding a changed field that is set whenever the atomic's value
+// is changed so that the iterators can decide whether or not they need to
+// recompute derived values. At present, this isn't needed because the iterators
+// are simple and checking the flag would probably be more costly than just
+// recomputing the values.
+//
+// If this flag was added, we could also add an on_change convenience method
+// that is called in the iterators, which clears the changed flag afterwards.
+// However, this would mean we couldn't reuse the same DynamicFloat in multiple
+// iterators because we'd mistakenly reset the flag before all iterators have
+// seen it. In that case, we could use generational indexing instead.
+
 #[derive(Clone)]
 pub struct DynamicFloat {
     pub value: Arc<AtomicF32>,
